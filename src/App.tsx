3b250c49ce45b7956ff668cf0e4603f46d4f0d6d
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-02 16:33:29 +0400

    create light mode with useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import Header from "./components/Header";
+import { useState } from "react";
+
 const App = () => {
-  return <div>App</div>;
+  const [LightMode, setLightMode] = useState(false);
+  return (
+    <div
+      className={`w-full h-screen flex justify-center ${
+        LightMode ? "bg-white" : "bg-[#171823]"
+      }`}
+    >
+      <Header LightMode={LightMode} setLightMode={setLightMode} />
+    </div>
+  );
 };
 
 export default App;
