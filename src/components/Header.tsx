05eff58281cb02ea6cb81514b7e4f559b1d53987
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-02 16:34:30 +0400

    create and complete Header.tsx
    create and complete Header with darkMode

---
+++ src/components/Header.tsx
@@ -0,0 +1,53 @@
+import Sun from "../assets/icon-sun.svg";
+import Moon from "../assets/icon-moon.svg";
+
+interface HeaderProps {
+  LightMode: boolean;
+  setLightMode: (e: boolean) => void;
+}
+
+const Header = ({ LightMode, setLightMode }: HeaderProps) => {
+  return (
+    // Main
+    <div className="w-full">
+      {/* Background */}
+      <div
+        className={`${
+          LightMode
+            ? "bg-DarkModeBackgroundMobile md:bg-DarkModeBackgroundDesktop"
+            : "bg-LightModeBackgroundMobile md:bg-LightModeBackgroundDesktop"
+        } h-52 md:h-80 bg-cover bg-center`}
+      >
+        {/* Header (navbar) */}
+        <div
+          className="flex flex-row justify-between md:items-center md:w-[541px] 
+            md:mx-auto px-6 pt-10 md:pt-20 md:px-0"
+        >
+          <h1
+            className="text-white text-2xl font-bold tracking-[10px] uppercase cursor-pointer
+            md:text-4xl md:tracking-[15px]"
+          >
+            todo
+          </h1>
+          {LightMode ? (
+            <img
+              onClick={() => setLightMode(!LightMode)}
+              src={Moon}
+              alt="SunIcon"
+              className="w-5 h-5 md:w-7 md:h-7 cursor-pointer"
+            />
+          ) : (
+            <img
+              onClick={() => setLightMode(!LightMode)}
+              src={Sun}
+              alt="MoonIcon"
+              className="w-5 h-5 md:w-7 md:h-7 cursor-pointer"
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
