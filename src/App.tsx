a48aaa0dd5ca78c7602537b8a061d2b5e67a32eb
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-03 02:19:15 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import Header from "./components/Header";
 import { useState } from "react";
+import Todos from "./components/Todos";
 
 const App = () => {
   const [LightMode, setLightMode] = useState(false);
   return (
     <div
-      className={`w-full h-screen flex justify-center ${
+      className={`w-full h-screen flex flex-col  ${
         LightMode ? "bg-white" : "bg-[#171823]"
       }`}
     >
       <Header LightMode={LightMode} setLightMode={setLightMode} />
+      <Todos LightMode={LightMode} />
     </div>
   );
 };
