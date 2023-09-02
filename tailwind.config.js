e600884bb6db74b0dc0bd5a79db6561585536eae
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-02 16:33:49 +0400

    add darkMode: "class" in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        LightModeBackgroundMobile: "url('./src/assets/bg-mobile-light.jpg')",
+        DarkModeBackgroundMobile: "url('./src/assets/bg-mobile-dark.jpg')",
+        LightModeBackgroundDesktop: "url('./src/assets/bg-desktop-light.jpg')",
+        DarkModeBackgroundDesktop: "url('./src/assets/bg-desktop-dark.jpg')",
+      },
+    },
   },
   plugins: [],
 };
