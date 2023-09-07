7833d170b153c1ea8e2d77019156abdae074b069
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-07 17:12:40 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,10 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        LightModeBackgroundMobile: "url('./src/assets/bg-mobile-light.jpg')",
-        DarkModeBackgroundMobile: "url('./src/assets/bg-mobile-dark.jpg')",
-        LightModeBackgroundDesktop: "url('./src/assets/bg-desktop-light.jpg')",
-        DarkModeBackgroundDesktop: "url('./src/assets/bg-desktop-dark.jpg')",
+        LightModeBackgroundMobile: "url('src/assets/bg-mobile-light.jpg')",
+        DarkModeBackgroundMobile: "url('/src/assets/bg-mobile-dark.jpg')",
+        LightModeBackgroundDesktop: "url('/src/assets/bg-desktop-light.jpg')",
+        DarkModeBackgroundDesktop: "url('/src/assets/bg-desktop-dark.jpg')",
       },
     },
   },
